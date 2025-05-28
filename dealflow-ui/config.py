c43060a90b6de5d280b765a685d5e795c81a1d41
Rishabh Sharma <rishabh.sharma@spinutech.com>
2025-05-28 10:56:28 -0400

    hehehe

--- dealflow-ui/config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-import os
-
-# API Configuration
-API_URL = os.environ.get('DEALFLOW_API_URL', 'http://localhost:8000')
-
-# Flask Configuration
-DEBUG = os.environ.get('FLASK_DEBUG', 'True') == 'True'
-SECRET_KEY = os.environ.get('FLASK_SECRET_KEY', 'dealflow_development_key')
